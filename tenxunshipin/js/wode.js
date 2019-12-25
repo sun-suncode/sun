09860c9889cf4cc2d8d3c93f922591827a5f6fe5
sunhuajian745 <sunhuajian1214@gmail.com>
2019-12-25 20:44:50 +0800

    zhousan

--- tenxunshipin/js/wode.js
+++ tenxunshipin/js/wode.js
@@ -1,23 +1,20 @@
 // 我的界面
-    var my_history = document.querySelector(".my_history")
+var my_history = document.querySelector(".my_history")
 
-    console.log(watch_history_date)
+console.log(watch_history_date)
 
-    var content_list3 = ""
-    for (var i = 0; i < watch_history_date.length; i++) {
+var content_list3 = ""
+for (var i = 0; i < watch_history_date.length; i++) {
 
 
-        content_list3 += `<div class="history">
+    content_list3 += `<div class="history">
                         <img src=`+ watch_history_date[i].img + ` "alt="">
                         <p>`+ watch_history_date[i].name + `<p>
 
                     </div>`
 
-    }
-    my_history.innerHTML = content_list3
-
-
-
+}
+my_history.innerHTML = content_list3
 
 
 
@@ -25,10 +22,10 @@
 
 
 // 我的看单
-    var kandan = document.querySelector(".kandan_list")
-    var content_list5 = ""
-    for (var i = 0; i < my_kandan_date.length; i++) {
-        content_list5 += ` <div class="one">
+var kandan = document.querySelector(".kandan_list")
+var content_list5 = ""
+for (var i = 0; i < my_kandan_date.length; i++) {
+    content_list5 += ` <div class="one">
                         <div class="kandan_img">
                             <img src=`+ my_kandan_date[i].img + ` alt="">
                             <div class="biaoshi">
@@ -43,5 +40,7 @@
 
                     </div>
 `
-    }
-    kandan.innerHTML = content_list5
+}
+kandan.innerHTML = content_list5
+
+
