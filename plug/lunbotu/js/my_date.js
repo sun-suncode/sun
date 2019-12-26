16e1f1ad06c3b178d1707bbe964d9c2d1b2da6ae
sunhuajian745 <sunhuajian1214@gmail.com>
2019-12-26 20:58:54 +0800

    插件练习

---
+++ plug/lunbotu/js/my_date.js
@@ -0,0 +1,50 @@
+var date = [
+    {
+        img: "./images/1.jpeg"
+    },
+    {
+        img: "./images/2.jpeg"
+    },
+    {
+        img: "./images/3.jpeg"
+    },
+    {
+        img: "./images/4.jpeg"
+    },
+    {
+        img: "./images/5.jpeg"
+    },
+    {
+        img: "./images/6.jpeg"
+    },
+    {
+        img: "./images/7.jpeg"
+    },
+    {
+        img: "./images/8.jpeg"
+    },
+    {
+        img: "./images/9.jpeg"
+    },
+    {
+        img: "./images/10.jpeg"
+    },
+    {
+        img: "./images/11.jpeg"
+    },
+    {
+        img: "./images/12.jpeg"
+    },
+    {
+        img: "./images/13.jpeg"
+    },
+    {
+        img: "./images/14.jpeg"
+    },
+    {
+        img: "./images/15.jpeg"
+    }
+
+
+]
+
